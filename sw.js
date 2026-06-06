0bb20d078739e12bcf32709c6b2f4108ea132e8f
spiele-spec <spiele@martinwolff.eu>
2026-06-07 01:33:56 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'chor-doelau-v1';
+const OFFLINE_FILES = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(OFFLINE_FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network first, fall back to cache
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
